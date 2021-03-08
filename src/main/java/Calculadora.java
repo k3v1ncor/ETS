2e163a2e04d00319bcdf3b6edf2a64a243888aa2
kevin <kevin@DESKTOP-3235GDN>
2021-03-08 18:33:15 +0100

    Paso 1 y 2

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kevin
+ */
+public class Calculadora {
+    private Integer operador1;
+    private Integer operador2;
+    private Integer operacion;
+    
+}
