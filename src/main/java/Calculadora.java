071277e3def19d05e9a315a6a3c475c5239dd5fa
kevin <kevin@DESKTOP-3235GDN>
2021-03-08 18:42:13 +0100

    Paso 2 y 3

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -12,5 +12,29 @@ public class Calculadora {
     private Integer operador1;
     private Integer operador2;
     private Integer operacion;
+
+    public Integer getOperador1() {
+        return operador1;
+    }
+
+    public void setOperador1(Integer operador1) {
+        this.operador1 = operador1;
+    }
+
+    public Integer getOperador2() {
+        return operador2;
+    }
+
+    public void setOperador2(Integer operador2) {
+        this.operador2 = operador2;
+    }
+
+    public Integer getOperacion() {
+        return operacion;
+    }
+
+    public void setOperacion(Integer operacion) {
+        this.operacion = operacion;
+    }
     
 }
